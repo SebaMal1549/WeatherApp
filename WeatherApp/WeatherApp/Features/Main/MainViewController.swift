55d8be21eb7310e22e3572bf09c90726c534e9bc
SebaMal1549 <sebastian.maludzinski@luxmed.pl>
2025-02-04 11:07:03 +0100

    Configuration of the project
    Configuration of the project

--- WeatherApp/WeatherApp/Features/Main/MainViewController.swift
+++ WeatherApp/WeatherApp/Features/Main/MainViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+    
+    // MARK: - API
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
     }
-
-
+    
 }
-
